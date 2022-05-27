ad21218790d1650393ea872596bf8cc15d053a51
tomikodes <startstickscephas@gmail.com>
2022-05-27 14:31:04 -0700

    ZuriTask (Fiding Anagrams)

---
+++ main.py
@@ -0,0 +1,22 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+def find_anagram(word, anagram):
+    # [assignment] Add your code here
+    word = word.lower()
+    anagram = anagram.lower()
+    if sorted(word) == sorted(anagram) :
+        return True
+    else :
+        return False
+
+find_anagram("hello", "check")
+
+
+
+
+
+
